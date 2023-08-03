9cc70ec94d82ebd68e210d262b1d70e26680badc
natapage <624832@gmail.com>
2023-08-03 12:59:15 +0300

    added home view image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
-<template></template>
+<template>
+  <NSpace justify="center" align="center">
+    <n-image
+      src="http://localhost:1337/uploads/large_1619763173_17_phonoteka_org_p_fon_dlya_saita_obrazovatelnogo_uchrezhdeni_19_664005d488.jpg"
+    ></n-image>
+  </NSpace>
+</template>
 
-<script setup></script>
+<script setup lang="ts">
+import { NImage, NSpace } from "naive-ui";
+</script>
 
 <style scoped></style>
