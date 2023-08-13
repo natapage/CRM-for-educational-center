e6cea4e6b2eaf27fe0b0c25648fd63eaca647725
natapage <624832@gmail.com>
2023-08-13 11:50:27 +0300

    deleted custom directive

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/router";
-import vClickOutside from "click-outside-vue3";
 
 const app = createApp(App);
 app.use(router);
-app.use(vClickOutside);
 app.mount("#app");
